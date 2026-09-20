8855578cdb188426df0052dfcea014bb751b14a5
Manx-08 <xxulu526@gmail.com>
2026-09-20 22:16:58 +0200

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive information.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -21,14 +21,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAqlQoFphkb-X0iqcM-4XFlOis0hTlhfKM",
-  authDomain: "healthcare-iot-monitoring-sys.firebaseapp.com",
-  projectId: "healthcare-iot-monitoring-sys",
-  storageBucket: "healthcare-iot-monitoring-sys.firebasestorage.app",
-  messagingSenderId: "290606890348",
-  appId: "1:290606890348:web:0483179e04fa3549124749",
-  measurementId: "G-3301S12DT6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
